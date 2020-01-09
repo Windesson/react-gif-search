f4e2e5b749ffde47c2372fbc2c7024b1f42517fb
Silva <windesson@hotmail.com>
2020-01-08 22:12:45 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A React app to display the GIF search results
+=============================================
+This apps fetchs gifs from https://giphy.com/ api interface. 
+Users can search for gifs by categories such as cats, dogs, sports and other using the search field.
---
+++ build/precache-manifest.defcdea049b3894a941a42fc236c51b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c07ba6da139ecf6541e06b8ae9b76424",
+    "url": "/react-gif-search/index.html"
+  },
+  {
+    "revision": "0af8235549cf7183ac83",
+    "url": "/react-gif-search/static/css/main.d6b7282e.chunk.css"
+  },
+  {
+    "revision": "adb6d7fafa08a691f1d4",
+    "url": "/react-gif-search/static/js/2.50e1e369.chunk.js"
+  },
+  {
+    "revision": "860bc5081fcd60d3c959d0d8166dd439",
+    "url": "/react-gif-search/static/js/2.50e1e369.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0af8235549cf7183ac83",
+    "url": "/react-gif-search/static/js/main.795dfcae.chunk.js"
+  },
+  {
+    "revision": "2603e2df7a277bf2d564",
+    "url": "/react-gif-search/static/js/runtime-main.3a0a2b9d.js"
+  }
+]);
