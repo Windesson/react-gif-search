89369513bcad9657eb7d71038b73f9b730949d79
Silva <windesson@hotmail.com>
2020-01-08 22:35:49 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
   }
 
   componentDidMount() {
-    axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    axios.get('https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
     .then(response => {
       this.setState({ gifs: response.data.data, loading: false});
     })
@@ -33,7 +33,7 @@ export default class App extends Component {
   // }
 
   performSearch = (query) => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
     .then(response => {
       this.setState({ gifs: response.data.data})
     })
